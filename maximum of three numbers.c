59d26e9e10fbfb2600a6f37fb9f722d7d3a8b0c9
chongaladurgabhavani3-boop <chongaladurgabhavani3@gmail.com>
2025-11-06 08:16:26 +0530

    Add files via upload

---
+++ maximum of three numbers.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+	int a,b,c,max;
+	printf("enter the three numbers:");
+	scanf("%d%d%d",&a,&b,&c);
+	max=(a>b)?((a>c)?a:c):((b>c)?b:c);
+	printf("the maximum number is:%d",max);
+	return 0;
+	
+}
